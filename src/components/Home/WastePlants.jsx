0861942631938008fef791f4eada548778eee64f
TridiziDev3 <tridizi.dev3@gmail.com>
2025-12-10 12:20:14 +0530

    changes d

--- src/components/Home/WastePlants.jsx
+++ src/components/Home/WastePlants.jsx
@@ -7,8 +7,8 @@ import { IoIosNavigate } from 'react-icons/io';
 // Placeholder image (check this path!)
 
 import plantImage1 from "../../assets/Homepage/Rectangle 34625677.png";
-import plantImage3 from "../../assets/Homepage/image 1.png";
-import plantImage2 from "../../assets/Homepage/image 2.png";
+import plantImage3 from "../../assets/Homepage/Image1.png";
+import plantImage2 from "../../assets/Homepage/Image2.png";
 
 
 const plantImages = [plantImage1, plantImage2, plantImage3];
