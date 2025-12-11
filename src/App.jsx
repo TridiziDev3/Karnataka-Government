5872c579fb227bf1c2eb6bce1cd110a0da9e0bd6
VISWESWARARAO-42 <visweswararasappa13@gmail.com>
2025-12-11 17:27:56 +0530

    latest chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 // src/App.jsx
 import React from "react";
-import Homepage from "./components/Homepage/Homepage";
 import "./index.css"; 
 import Home from "./components/Home/Home";
-
 
 const App = () => {
   return (
